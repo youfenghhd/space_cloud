1abdf561e6acb830267bf376a65d456b15661272
hhd <1610766552@qq.com>
2023-02-16 00:25:21 +0800

    mybatis—plus代码生成器生成模板

---
+++ src/test/java/com/hhd/SpaceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
