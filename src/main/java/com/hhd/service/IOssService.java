24dc1941124814d7e3428941f5acda6f26afffcf
hhd <1610766552@qq.com>
2023-02-19 22:18:33 +0800

    完善后台功能

---
+++ src/main/java/com/hhd/service/IOssService.java
@@ -0,0 +1,8 @@
+package com.hhd.service;
+
+/**
+ * @author -无心
+ * @date 2023/2/19 22:17:39
+ */
+public class IOssService {
+}
---
+++ src/main/java/com/hhd/service/impl/OssServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hhd.service.impl;
+
+/**
+ * @author -无心
+ * @date 2023/2/19 22:18:00
+ */
+public class OssServiceImpl {
+}
